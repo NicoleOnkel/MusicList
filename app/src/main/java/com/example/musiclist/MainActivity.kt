ce4c6831a76e97bee1299c578d47a76e8fc3db97
NicoleOnkel <ST10497522@rcconnect.edu.za>
2025-06-19 10:18:41 +0200

    add

--- app/src/main/java/com/example/musiclist/MainActivity.kt
+++ app/src/main/java/com/example/musiclist/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.musiclist
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Layout
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,6 +23,40 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         val addonlist = findViewById<Button>(R.id.addtolist)
+        val textbox = findViewById<EditText>(R.id.inputbox)
+        val saveItems = findViewById<Button>(R.id.saveitem)
+
+
+
+        //Arrays
+        val TheSongTitle = mutableListOf<String>()
+        val TheArtistName = mutableListOf<String>()
+        val TheRating = mutableListOf<Int>()
+        val UserComments = mutableListOf<String>()
+
+        addonlist.setOnClickListener {
+            if (addonlist.isEnabled == true)
+                textbox.visibility = View.VISIBLE
+            saveItems.visibility = View.VISIBLE
+
+
+
+
+        }
+
+        saveItems.setOnClickListener {
+
+            var frominputbox =textbox.text.toString()
+            if(frominputbox.isNotEmpty())
+                TheSongTitle.add(frominputbox)
+                    textbox.text.clear()//clear text
+
+
+        }
+
+
+
+
 
 
 
