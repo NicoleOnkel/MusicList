b7cb2cd234bea6db9b5c60fb8d96e5b0c8ffd639
NicoleOnkel <ST10497522@rcconnect.edu.za>
2025-06-19 09:41:27 +0200

    add

--- app/src/main/java/com/example/musiclist/MainActivity.kt
+++ app/src/main/java/com/example/musiclist/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.musiclist
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val addonlist = findViewById<Button>(R.id.addtolist)
+
+
+
     }
 }
